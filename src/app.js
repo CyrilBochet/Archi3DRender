973b1720ed774be743259b551481c9ce3d4b2ec8
Cyril BOCHET <bochet66@gmail.com>
2023-07-16 18:30:46 +0200

    wip

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import 'owl.carousel/dist/assets/owl.carousel.min.css'
 import 'owl.carousel/dist/assets/owl.theme.default.min.css'
 import 'owl.carousel/dist/owl.carousel.min'
 import './scripts/main';
+import './scripts/contact-form';
+
 
 import './css/styles.css';
 import './css/material-symbols.css';
